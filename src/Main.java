1a774fddc05d0a5f0a66473eef93da5893515fdc
MaximSilva <58977585+MaximSilva@users.noreply.github.com>
2023-08-31 11:28:00 +0300

    MainClass
    This code splits the input file into commands and processes them, updating the order book and generating responses. The results are written to the output file "output.txt". The assignment requires you to create a .jar executable with this code and use the "input.txt" file as an input file and generate the output file "output.txt".

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,54 @@
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
+            BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
+
+            BookOrder orderBook = new BookOrder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",");
+                String command = parts[0];
+                if (command.equals("u")) {
+                    String orderType = parts[3];
+                    int price = Integer.parseInt(parts[1]);
+                    int size = Integer.parseInt(parts[2]);
+                    orderBook.update(orderType, price, size);
+                } else if (command.equals("q")) {
+                    if (parts[1].equals("best_bid")) {
+                        String result = orderBook.getBestPriceAndSize("bid");
+                        if (!result.isEmpty()) {
+                            writer.write(result);
+                            writer.newLine();
+                        }
+                    } else if (parts[1].equals("best_ask")) {
+                        String result = orderBook.getBestPriceAndSize("ask");
+                        if (!result.isEmpty()) {
+                            writer.write(result);
+                            writer.newLine();
+                        }
+                    } else if (parts[1].equals("size")) {
+                        int price = Integer.parseInt(parts[2]);
+                        orderBook.removeBids(price);
+                        orderBook.removeAsks(price);
+                    }
+                } else if (command.equals("o")) {
+                    String orderType = parts[1];
+                    int size = Integer.parseInt(parts[2]);
+                    if (orderType.equals("buy")) {
+                        orderBook.removeAsks(size);
+                    } else if (orderType.equals("sell")) {
+                        orderBook.removeBids(size);
+                    }
+                }
+            }
+
+            reader.close();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
