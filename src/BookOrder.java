fd1dcbf98b8309e96d3f33658daa4d58aa816016
MaximSilva <58977585+MaximSilva@users.noreply.github.com>
2023-08-31 11:08:44 +0300

    Class BookOrder
    Create class BookOrder for model the order book for your task. An order book is a key component of stock exchange systems that stores information about the shares that investors are willing to sell (ask) and buy (bid).

---
+++ src/BookOrder.java
@@ -0,0 +1,24 @@
+import java.util.Collections;
+import java.util.TreeMap;
+
+public class BookOrder {
+    private TreeMap<Integer, Integer> bids; // purchase order book
+    private TreeMap<Integer, Integer> asks; //book of sales leads
+
+    public BookOrder(){
+        bids = new TreeMap<>(Collections.reverseOrder());
+        asks = new TreeMap<>();
+    }
+
+
+    //Update of the book of orders
+    public void update(String orderType, int price, int size)
+    {
+        if(orderType.equals("bid")){
+            bids.put(price, size);
+        }
+        else if(orderType.equals("ask")){
+            asks.put(price, size);
+        }
+    }
+}
