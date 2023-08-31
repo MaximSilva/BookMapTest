67eb03facd64628f9dfff1e35cb194c96bf81d68
MaximSilva <58977585+MaximSilva@users.noreply.github.com>
2023-08-31 11:18:30 +0300

    Update to BookOrder
    Add 3 methods for best price sales, order and get the best price and size

--- src/BookOrder.java
+++ src/BookOrder.java
@@ -1,4 +1,6 @@
 import java.util.Collections;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.TreeMap;
 
 public class BookOrder {
@@ -20,5 +22,52 @@ public class BookOrder {
         else if(orderType.equals("ask")){
             asks.put(price, size);
         }
+    }
+
+    // Delete shares from the buy order book
+    public void removeBids(int size){
+        Iterator<Map.Entry<Integer, Integer>> iterator = bids.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<Integer, Integer> entry = iterator.next();
+            int price = entry.getKey();
+            int currentSize = entry.getValue();
+            if(currentSize <= size){
+                iterator.remove();
+                size -= currentSize;
+            }
+            else {
+                bids.put(price, currentSize - size);
+                break;
+            }
+        }
+
+    }
+    // Delete shares from the sales order book
+    public void removeAsks(int size) {
+        Iterator<Map.Entry<Integer, Integer>> iterator = asks.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<Integer, Integer> entry = iterator.next();
+            int price = entry.getKey();
+            int currentSize = entry.getValue();
+            if (currentSize <= size) {
+                iterator.remove();
+                size -= currentSize;
+            } else {
+                asks.put(price, currentSize - size);
+                break;
+            }
+        }
+    }
+
+    //Get the best price and size
+    public String getBestPriceAndSize(String type) {
+        TreeMap<Integer, Integer> book = type.equals("bid") ? bids : asks;
+        if (book.isEmpty()) {
+            return "";
+        }
+        Map.Entry<Integer, Integer> bestEntry = book.firstEntry();
+        int price = bestEntry.getKey();
+        int size = bestEntry.getValue();
+        return price + "," + size;
     }
 }
